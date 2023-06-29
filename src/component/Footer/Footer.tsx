303a4e68e76017a3987bdb5ba2c06030037c994a
felipelamela <fe-fl@outlook.com>
2023-06-29 18:53:34 -0300

    Texto Footer

--- src/component/Footer/Footer.tsx
+++ src/component/Footer/Footer.tsx
@@ -1,3 +1,57 @@
-export default function Footer(){
-  return <footer>Footer</footer>
+
+
+
+export default function Footer() {
+  return (<footer>
+    <div>
+      <img src="/img/logo.png" alt="" />
+      <p>Transformando visitantes em clientes.</p>
+    </div>
+
+    <div>
+      <div>
+        <h3>Links Principais</h3>
+        <div>
+          <p>Home</p>
+          <p>Ferramenta</p>
+          <p>Preços</p>
+          <p>Contato</p>
+        </div>
+      </div>
+      <div>
+        <h3>Cases</h3>
+        <div>
+          <p>Geração de Leads B2B</p>
+          <p>Geração de Leads em Software</p>
+          <p>Geração de Leads em Imobiliária</p>
+          <p>Cases de Sucesso</p>
+        </div>
+      </div>
+      <div>
+        <h3>Materias</h3>
+        <div>
+          <p>Blog</p>
+          <p>Parcerias com Agências</p>
+          <p>Guia Definitivo do Marketing</p>
+          <p>Materiais Gratuitos</p>
+        </div>
+      </div>
+      <div>
+        <h3>Siga a Leadster</h3>
+        <div>
+          <p>links </p>
+          <p></p>
+          <p>Email</p>
+          <p>Telefone</p>
+        </div>
+      </div>
+    </div>
+
+
+    <div>
+      <p>Copyright</p>
+      <p>local</p>
+
+    </div>
+  </footer>)
 }
