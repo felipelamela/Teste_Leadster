9654073036b1a139d9c17777afada2a44c4de138
felipelamela <fe-fl@outlook.com>
2023-07-01 14:01:34 -0300

    paginator

--- src/component/CardSec/CardSec.tsx
+++ src/component/CardSec/CardSec.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import CardVideo from '../CardVideo/CardVideo'
 import style from './CardSec.module.css'
+import { NextPage } from 'next'
 
 interface VideoCard {
   id: number,
@@ -14,40 +15,77 @@ interface VideoCard {
 }
 
 
-const CardSec: React.FC = () => {
-  const [videoCard, setVideoCard] = React.useState<VideoCard[] | null>(null)
+const CardSec: NextPage = () => {
+  //CardVideos
+  const [videoCard, setVideoCard] = React.useState<VideoCard[]>([])
+  const [boxVideo, setBoxVideo] = React.useState<VideoCard[]>([])
 
+  //paginator
+  const [pagina, setPagina] = React.useState<number[]>([])
+  const [numeroPagina, setNumeroPagina] = React.useState(0)
+  const cardPerPagina: number = 9
+  const start = ((numeroPagina - 1) * cardPerPagina)
+  const end = start + cardPerPagina
+
+
+  //fetch da api
   React.useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch('/data/videos.json')
         const data = await response.json();
         setVideoCard(data)
+        setNumeroPagina(1)
       } catch (error) {
         console.error("Erro ao acessar dados do JSON", error)
       }
     }
-
     fetchData()
   }, [])
 
 
 
-  if (!videoCard) return <>Carregando...</>
+  //Regando numeros paginação
+  React.useEffect(() => {
+    const paginaFinal = Math.ceil(videoCard.length / 9)
+    for (let index = 1; index <= paginaFinal; index++) {
+      setPagina((pagina) => [...pagina, index])
+    }
+  }, [videoCard])
+
+
+
+  //Videos do Box
+  React.useEffect(() => {
+    const boxVideo = videoCard.slice(start, end)
+    setBoxVideo(() => boxVideo)
+  }, [numeroPagina])
+
+
+
+
+  // Carregando --- Colocar svg rotativo
+  if (videoCard.length === 0) return <>Carregando...</>
 
   return (
-    <section className={style.containerCards}>
+    <section >
+      <div className={style.containerCards}>
 
-      {videoCard.map((video, index) => (
+        {boxVideo.map((video, index) => (
+          <CardVideo
+            key={index}
+            id={video.id}
+            titulo={video.titulo}
+            thumbnail={video.thumbnail}
+          />
+        ))}
 
-        <CardVideo
-          key={index}
-          id={video.id}
-          titulo={video.titulo}
-          thumbnail={video.thumbnail}
-
-        />
-      ))}
+      </div>
+      <div>
+        {pagina.map((valor) => (
+          <button onClick={() => setNumeroPagina(valor)} key={valor}>{valor}</button>
+        ))}
+      </div>
 
     </section>
   )
